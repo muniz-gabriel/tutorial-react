e0f9ae1a48ca6a0ae838d2df77698e6d2f93a48d
muniz-gabriel <muniz.gabriel@aluno.ifsp.edu.br>
2025-08-05 16:26:29 -0300

    ☑️Initial Commit✅

--- App.js
+++ App.js
@@ -4,7 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.box1}></View>
+      <Text>Tutorial criando App com Expo - CARMOB IFSP</Text>
+      <h2>React Native</h2>
+      <Text style={styles.tituloPrincipal}>Título Principal</Text>
+      <Text style={styles.subTitulo}>Subtítulo</Text>
+      <Text>Este é um parágrafo</Text>
+      <Text style={styles.saibaMais}>Saiba mais</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +23,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  tituloPrincipal: {
+    fontSize: '20px',
+    fontWeight: 'bold',
+    marginVertical: 20,
+  },
+  subTitulo: {
+    fontSize: '18px',
+    fontWeight: 'bold',
+    marginVertical: 20,
+  },
+  saibaMais: {
+    fontSize: '16px',
+    color: "#464646",
+  },
+  box1: {
+    width: "100%",
+    heght: 80,
+    backgroundColor: "#",
+  },
+  box2: {
+
+  }
 });
---
+++ README.md
@@ -0,0 +1,13 @@
+# Comandos Úteis
+
+- npx create-expo-app@latest --template //cria o projeto React Native com Expo
+
+# Comandos Expo
+
+- npm start 
+- npm expo start --tunnel
+
+# Comandos Git 
+-git config --global user.name "Gabriel Muniz"
+-git config --global user.email "muniz.gabriel@aluno.ifsp.edu.br"
+
